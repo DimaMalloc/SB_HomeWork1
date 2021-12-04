5ec95e306b715bc508aea6f1c275755fce75553e
Dima Balabanov <nuez1983@yandex.ru>
2021-12-04 21:18:26 +0300

    Добавьте файлы проекта.

---
+++ SB_HomeWork1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace SB_HomeWork1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //Установите IDE Visual Studio. 
+            //Создайте новое консольное приложение.
+            //Добавьте оператор вывода на экран Console.WriteLine и с помощью метода С помощью метода Console.WriteLine Console.WriteLine выведите надпись: Hello World!!!
+            //Добавьте задержку Console.ReadKey, чтобы приложение не закрывалось по завершении выполнения.
+
+            Console.WriteLine("Hello World!");
+            Console.ReadKey();
+
+            //Выведите на экран надпись Hello World!!!, используя метод Console.Write.
+            //Используйте Console.Write для каждого слова(у вас будет три вызова метода: сначала для слова Hello, потом для слова World, а затем — для трёх восклицательных знаков).
+            //Чтобы слова не «слипались», используйте пробелы внутри кавычек.
+            //Добавьте задержку Console.ReadLine, чтобы приложение не закрывалось по завершении выполнения.
+
+            Console.Write("Hello ");
+            Console.Write("World ");
+            Console.Write("!!!");
+            Console.ReadKey();
+        }
+    }
+}
